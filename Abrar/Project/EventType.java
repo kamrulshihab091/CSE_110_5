f77fd456d191e43f078ea0a72118686e786de9e9
kamrul Islam <136752808+kamrulshihab091@users.noreply.github.com>
2023-08-19 05:25:13 +0000

    Brooh done

---
+++ Abrar/Project/EventType.java
@@ -0,0 +1,31 @@
+
+public class EventType {
+    private int typeID;
+    private String name;
+    private String description;
+
+    public int getTypeID() {
+        return typeID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTypeID(int typeID) {
+        this.typeID = typeID;
+    }
+
+}
